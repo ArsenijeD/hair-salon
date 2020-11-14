c3f172e86b53bd3c27394c1a2b2f8797db67380b
ArsenijeD <aco.degenek@gmail.com>
2020-11-14 15:58:44 +0100

    Add facade for authenticated user

--- hair-salon/src/main/java/com/example/hairsalon/controller/ReservationController.java
+++ hair-salon/src/main/java/com/example/hairsalon/controller/ReservationController.java
@@ -1,8 +1,10 @@
 package com.example.hairsalon.controller;
 
 import com.example.hairsalon.dto.ReservationDTO;
+import com.example.hairsalon.security.util.AuthenticationFacade;
 import com.example.hairsalon.service.ReservationService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,11 @@ public class ReservationController {
 
     private final ReservationService reservationServiceImpl;
 
-    public ReservationController(ReservationService reservationServiceImpl) {
+    private final AuthenticationFacade authenticationFacadeImpl;
+
+    public ReservationController(ReservationService reservationServiceImpl, AuthenticationFacade authenticationFacadeImpl) {
         this.reservationServiceImpl = reservationServiceImpl;
+        this.authenticationFacadeImpl = authenticationFacadeImpl;
     }
 
 
---
+++ hair-salon/src/main/java/com/example/hairsalon/security/util/AuthenticationFacade.java
@@ -0,0 +1,7 @@
+package com.example.hairsalon.security.util;
+
+import org.springframework.security.core.Authentication;
+
+public interface AuthenticationFacade {
+    Authentication getAuthentication();
+}
