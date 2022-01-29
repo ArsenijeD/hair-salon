d82e113c18f900fdd70d435cc7b2fa5bbc5cbe8c
Filip Kostic <filip.kostic@theinfatuation.com>
2022-01-29 22:18:03 +0100

    Format reservation date without timezone

--- hair-salon-platform-web/components/Reservations/ReservationForm/index.tsx
+++ hair-salon-platform-web/components/Reservations/ReservationForm/index.tsx
@@ -114,18 +114,9 @@ const ReservationForm: FC<ReservationFormProps> = ({ onSuccess, onClose }) => {
   );
 
   const onSubmit = (values: NewReservation) => {
-    // Has to be +1 (we aren't considering timezone), also cut 'Z' from final string
-    const hour = Number(values.startTime.split(":")[0]) + 1;
-    const minut = Number(values.startTime.split(":")[1]);
-
     const data = {
       customer: values.customer,
-      date: dayjs(date)
-        .hour(hour)
-        .minute(minut)
-        .second(0)
-        .toISOString()
-        .slice(0, -1),
+      date: `${dayjs(date).format("YYYY-MM-DD")}T${values.startTime}`,
       durationMinutes: 15,
       typeOfService: values.typeOfService,
       worker: workersData?.data.find((worker) => worker.id === values.worker),
